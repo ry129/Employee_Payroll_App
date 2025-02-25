51b2582d2514022ebcc16c8d5689241734fa5121
ry129 <0111cs211146.riya@gmail.com>
2025-02-25 15:59:44 +0530

    [Riya] ADD . added controllers

---
+++ src/main/java/com/employeepayrollappsetup/EmployeePayrollAppSetUpApplication.java
@@ -0,0 +1,13 @@
+package com.employeepayrollappsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppSetUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollAppSetUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/employeepayrollappsetup/EmployeePayrollAppSetUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeepayrollappsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppSetUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
