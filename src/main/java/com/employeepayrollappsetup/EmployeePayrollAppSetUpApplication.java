cc466a545b02d2fd562dd344d469590d7ca23eb3
ry129 <0111cs211146.riya@gmail.com>
2025-03-07 12:33:05 +0530

    [Riya]Add. logging levels of lombok added

--- src/main/java/com/employeepayrollappsetup/EmployeePayrollAppSetUpApplication.java
+++ src/main/java/com/employeepayrollappsetup/EmployeePayrollAppSetUpApplication.java
@@ -1,13 +1,16 @@
 package com.employeepayrollappsetup;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppSetUpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeePayrollAppSetUpApplication.class, args);
+        log.info("Employee Payroll App Started");
     }
 
 }
